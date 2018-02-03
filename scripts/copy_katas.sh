3df7c79e8a56f25dbbd358fe9099149e3a2c2de0
James Grenning <james@grenning.net>
2018-02-03 13:29:50 -0500

    Added script to copy katas from cyber-dojo-storer

--- scripts/copy_katas.sh
+++ scripts/copy_katas.sh
@@ -3,6 +3,10 @@ if [[ $# != 1 || ${#1} -ne 10 ]] ; then
   exit 1
 fi
 exercise=$1
+mkdir -p kata-capture/$exercise
 dir1=${exercise:0:2}
 dir2=${exercise:2:8}
-docker cp cyber-dojo-storer:/usr/src/cyber-dojo/katas/$dir1/$dir2 kata-capture/$exercise
+docker cp 'cyber-dojo-storer:/usr/src/cyber-dojo/katas/$dir1/$dir2/' kata-capture/
+pushd kata-capture
+mv $dir2 $exercise
+popd
