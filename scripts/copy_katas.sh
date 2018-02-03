9937bac242fb31d8ddb39e6bd67c1db22cb01f0e
James Grenning <james@grenning.net>
2018-02-03 18:37:58 +0000

    fixed kata-capture script

--- scripts/copy_katas.sh
+++ scripts/copy_katas.sh
@@ -3,7 +3,7 @@ if [[ $# != 1 || ${#1} -ne 10 ]] ; then
   exit 1
 fi
 exercise=$1
-mkdir -p kata-capture/$exercise
+mkdir -p kata-capture
 dir1=${exercise:0:2}
 dir2=${exercise:2:8}
 docker cp cyber-dojo-storer:/usr/src/cyber-dojo/katas/$dir1/$dir2/ kata-capture/
