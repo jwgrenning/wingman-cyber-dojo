4071ccb519f52644a65a5158cb6a6b6a48613255
James Grenning <james@grenning.net>
2018-02-09 13:15:22 +0100

    Update the update in cyber dojo script

--- scripts/update-start-points-in-cyber-dojo.sh
+++ scripts/update-start-points-in-cyber-dojo.sh
@@ -5,15 +5,17 @@ if [ $# != 1 ] ; then
   get_out=YES
 else
 
-if [ ! -f "start_point_type.json" ] ; then
-  echo "Run from the root of the start point"
-  get_out=YES
-fi
+  if [ ! -f "start_point_type.json" ] ; then
+    echo "Run from the root of the start point"
+    get_out=YES
+  fi
 
-if [ get_out == NO ] ; then
-  ../cyber-dojo down
-  ../cyber-dojo start-point rm $1
-  ../cyber-dojo start-point create $1 --dir=${PWD}
-  ../cyber-dojo start-point ls
-  ../cyber-dojo up --custom=$1
+  if [ get_out == NO ] ; then
+    ../cyber-dojo down
+    ../cyber-dojo start-point rm $1
+    ../cyber-dojo start-point create $1 --dir=${PWD}
+    ../cyber-dojo start-point ls
+    ../cyber-dojo up --custom=$1
+  fi
+
 fi
