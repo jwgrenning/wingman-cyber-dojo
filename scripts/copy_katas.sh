f57b5eb3cbeedd584df1d33b5abb630484439096
James Grenning <james@grenning.net>
2018-02-24 14:23:05 -0500

    Adding a file for kata-ids and modift copy_katas.sh to use use the list of files.

--- scripts/copy_katas.sh
+++ scripts/copy_katas.sh
@@ -3,17 +3,24 @@ if [ ! -f "start_point_type.json" ] ; then
   exit 1
 fi
 
-if [[ $# != 1 || ${#1} -ne 10 ]] ; then
-  echo "usage $0 AB12345678"
-  exit 1
-fi
-exercise=$1
+exercises=""
+while read -r line || [[ -n "$line" ]]; do
+    echo "Text read from file: $line"
+    id=$(echo $line | cut -d',' -f1)
+    exercises=$exercises $id
+done < "scripts/kata-ids.csv"
+
+echo Exercises: $exercises
+
 KATA_DIR=../kata-capture
 mkdir -p $KATA_DIR
-dir1=${exercise:0:2}
-dir2=${exercise:2:8}
-docker cp cyber-dojo-storer:/usr/src/cyber-dojo/katas/$dir1/$dir2/ $KATA_DIR
-pushd $KATA_DIR
-rm -rf $exercise
-mv $dir2 $exercise
-popd
+
+for exercise in exercises; do
+  dir1=${exercise:0:2}
+  dir2=${exercise:2:8}
+  docker cp cyber-dojo-storer:/usr/src/cyber-dojo/katas/$dir1/$dir2/ $KATA_DIR
+  pushd $KATA_DIR
+  rm -rf $exercise
+  mv $dir2 $exercise
+  popd
+done
