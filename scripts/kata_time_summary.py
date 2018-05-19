be356f94d9d76dc11828ff8f6cb07af7c6313238
James Grenning <james@grenning.net>
2018-05-19 11:10:13 -0500

    Add text to bottom of cvs file for original url and test run outcome

--- scripts/kata_time_summary.py
+++ scripts/kata_time_summary.py
@@ -63,6 +63,10 @@ def main(kata_capture_dir, kata_output_dir):
       output = open(output_filename + ".csv", "w")
       output.write(output_filename + ", signal, date, time, seconds, minutes, total seconds, total minutes, discontinuity\n")
       output.write(lines)
+      output.write("\nOriginal, kata: http://research.wingman-sw.com/kata/edit/" + exercise[:exercise.find('-')] + "?avatar=" + animal + "\n")
+      output.write("Passes tests\n")      
+      output.write("Fails tests: #\n")      
+      output.write("\n")      
       output.close()
 
 if __name__ == '__main__':
